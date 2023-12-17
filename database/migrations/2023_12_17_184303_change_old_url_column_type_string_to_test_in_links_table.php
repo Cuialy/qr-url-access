0fafa7328d9c9d1e0b1e435cedc216745f223d99
flysquare <umutkonurinso@gmail.com>
2023-12-17 21:43:26 +0300

    changed old_url type

---
+++ database/migrations/2023_12_17_184303_change_old_url_column_type_string_to_test_in_links_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->text('old_url')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->string('old_url')->change();
+        });
+    }
+};
